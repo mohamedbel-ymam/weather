425a62ffa7bd9104a7086b6ba6b775a7d5f5dcaf
mohamedbel-ymam <72866701+mohamedbel-ymam@users.noreply.github.com>
2022-03-24 17:45:14 +0100

    weather

---
+++ script.js
@@ -0,0 +1,72 @@
+let weather = {
+    apiKey: "88aa00f3ae00d8ff5c31b2354e84f2da",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Khouribga");
+
+
+
+// const successCallBack= (position)=>{
+//     console.log(position);
+// }
+// const errorCallBack =(error)=>{
+//     console.log(error);
+// }
+
+// navigator.geolocation.getCurrentPosition(successCallBack,errorCallBack)
+// const successFullLookUp=(position)=>{
+//     const{latitude,longitude}=position.coords;
+//     fetch('https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=101ec998e2b547b0b64a293edbb446d7')
+//     .then(Response=Response.json())
+//     .then(console.log) 
+// }
+
